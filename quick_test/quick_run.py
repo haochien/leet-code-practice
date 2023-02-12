02ae0cb17cdada664f5d310e2658d92ee0e1180c
hao <haochien886@gmail.com>
2023-02-12 17:02:20 +0100

    Q70 update

--- quick_test/quick_run.py
+++ quick_test/quick_run.py
@@ -1,16 +1,16 @@
-from typing import List
 
-def test_run(strs: List[str]) -> str:
-    result = ''
-    for letter_set in zip(*strs):
-        if len(set(letter_set)) == 1:
-            result += letter_set[0]
-        else:
-            break
+def test_run(n):
+    if n <=2:
+        return n
+    
+    res = [-1] * n
+    res[0], res[1] = 1, 2
 
-    return result
+    for i in range(2, n):
+        res[i] = res[i-1] + res[i-2]
 
+    return res[-1]
 
 if __name__ == '__main__':
-    result = test_run(["flower","flow","flight"])
+    result = test_run(4)
     print(f"result: {result}")
