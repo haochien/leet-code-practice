1f2a6390c7af120814043e7afeb3ad5d9aa22a82
hao <haochien886@gmail.com>
2023-02-13 22:18:53 +0100

    Q88 update

--- problems/two_pointers/essential/ez__merge_sorted_array.py
+++ problems/two_pointers/essential/ez__merge_sorted_array.py
@@ -36,7 +36,7 @@ class MergeSortedArray:
     @staticmethod
     def great_ans1( nums1: List[int], m: int, nums2: List[int], n: int) -> None:
         """
-        technique: Top-Down DP (dynamic programming)
+        technique: two pointer
         Time: O(n) 
         Memory: O(1)
         """
@@ -50,6 +50,32 @@ class MergeSortedArray:
         if n > 0:
             nums1[:n] = nums2[:n]
 
-        return nums1
+        print(nums1)
+        return
 
+
+    @staticmethod
+    def my_solution( nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        technique: two pointer
+        Time: O(n) 
+        Memory: O(1)
+        """
+        curr = m + n
+        m -= 1
+        n -= 1
+        while m >= 0 and n >= 0:
+            curr -= 1
+            if nums1[m] >= nums2[n]:
+                nums1[curr] = nums1[m]
+                m -= 1
+            else:
+                nums1[curr] = nums2[n]
+                n -= 1
+            
+        if m < n:
+            nums1[:curr] = nums2[:n+1]
+
+        print(nums1)
+        return
 
--- quick_test/quick_run.py
+++ quick_test/quick_run.py
@@ -1,8 +1,24 @@
 
-def test_run(nums):
+def test_run(nums1, m, nums2, n):
+    curr = m + n
+    m -= 1
+    n -= 1
+    while m >= 0 and n >= 0:
+        curr -= 1
+        if nums1[m] >= nums2[n]:
+            nums1[curr] = nums1[m]
+            m -= 1
+        else:
+            nums1[curr] = nums2[n]
+            n -= 1
+        
 
+    if m < n:
+        nums1[:curr] = nums2[:n+1]
+
+    print(nums1)
     return
 
 if __name__ == '__main__':
-    result = test_run([2,7,9,3,1])
+    result = test_run(nums1 = [0], m = 0, nums2 = [1], n = 1)
     print(f"result: {result}")
